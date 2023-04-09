109e4e937b9f4a08aa2198f5db00ebd6a0fb27eb
Connor <cnwilson29@gmail.com>
2023-04-09 05:10:39 +0100

    having fun!

---
+++ camera_control/camera_control.py
@@ -0,0 +1,57 @@
+import cv2
+import time
+from multiprocessing import Process, Value, Pipe
+import numpy as np
+
+class Camera:
+    def __init__(self, matrix_coefficients, distortion_coefficients, aruco_dict_type = cv2.aruco.DICT_6X6_50, image_dir = "/home/connor/Desktop/AquaTitan/images/"):
+        self.matrix_coefficients = matrix_coefficients
+        self.distortion_coefficients = distortion_coefficients
+        self.aruco_dict_type = aruco_dict_type
+        self.image_dir = image_dir
+        
+    def camera_process(self, camera_on, conn):
+        cap = cv2.VideoCapture(0)
+
+        while camera_on.value:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            conn.send(frame)
+            time.sleep(0.01)
+
+        cap.release()
+        
+    def initialize_camera(self):
+        self.camera_on = Value('i', 1)
+        self.parent_conn, child_conn = Pipe()
+        self.p = Process(target=self.camera_process, args=(self.camera_on, child_conn))
+        self.p.start()
+        print("Camera initialized")
+        
+    def stop(self):
+        self.camera_on.value = 0
+        self.p.join()
+        
+    def get_frame(self):
+        return self.parent_conn.recv()
+        
+    def get_position(self):
+        frame = self.get_frame()
+        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.aruco_dict = cv2.aruco.Dictionary_get(self.aruco_dict_type)
+        parameters = cv2.aruco.DetectorParameters_create()
+
+
+        corners, ids, _ = cv2.aruco.detectMarkers(gray, cv2.aruco_dict,parameters=parameters,
+            cameraMatrix=self.matrix_coefficients,
+            distCoeff=self.distortion_coefficients)
+
+        if len(corners) > 0:
+            res = []
+            for i in range(0, len(ids)):
+                rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(
+                    corners[i], 0.02, self.matrix_coefficients, self.distortion_coefficients)
+                res.append((ids[i], rvec, tvec))
+            return res
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from motor_control.motor_control import Motor
+from camera_control.camera_control import Camera
 from time import sleep
 
-if __name__ == "__main__":
+import numpy as np
+
+def test_motor():
     left = Motor(pin=4, debug=True)
     left.initialize_motor()
     left.set_power(0.5)
@@ -9,3 +12,13 @@ if __name__ == "__main__":
     left.set_power(-0.5)
     sleep(4)
     left.stop()
+    
+def test_camera():
+    camera = Camera(0, 0)
+    # # camera.initialize_camera(0, 0)
+    # camera.save_image("test.jpeg")
+    camera.initialize_camera()
+
+if __name__ == "__main__":
+    # test_motor()
+    test_camera()
